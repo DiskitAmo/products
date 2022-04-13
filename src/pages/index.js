63d7dd6de2fa2e05216749f70b437341344af7e0
diskit <diskit.angmo@catalyst.sh>
2022-04-13 18:48:33 +0530

    edit files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import { useEffect, useState } from "react"
-//import Layout from "../components/layout"
+
 import Card from "@mui/material/Card"
 import CardContent from "@mui/material/CardContent"
 import CardMedia from "@mui/material/CardMedia"
